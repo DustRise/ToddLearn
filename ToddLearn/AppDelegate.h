4e017cb9710053dc9161c4e60af991604ace22fb
sukrutha <sukrutha@dustrise.com>
2013-04-04 12:32:54 +0530

    Initial Commit

---
+++ ToddLearn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToddLearn
+//
+//  Created by Dustrise on 4/4/13.
+//  Copyright (c) 2013 Dustrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToddLearn/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToddLearn
+//
+//  Created by Dustrise on 4/4/13.
+//  Copyright (c) 2013 Dustrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
